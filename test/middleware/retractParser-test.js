9675868c254246d7c1e5c71407443844d87d18fe
Jared Hanson <jaredhanson@gmail.com>
2011-09-29 23:44:57 -0700

    Improve validation of retract requests.

--- test/middleware/retractParser-test.js
+++ test/middleware/retractParser-test.js
@@ -22,6 +22,7 @@ vows.describe('retractParser').addBatch({
         var iq = new IQ('pubsub.shakespeare.lit', 'hamlet@denmark.lit/elsinore', 'set');
         var pubsubEl = new PubSub();
         var retractEl = new Retract('princely_musings');
+        retractEl.c('item', { id: 'bnd81g37d61f49fgn581' });
         iq.c(pubsubEl).c(retractEl);
         iq = iq.toXML();
         iq.type = iq.attrs.type;
@@ -39,6 +40,9 @@ vows.describe('retractParser').addBatch({
       },
       'should set node property' : function(err, stanza) {
         assert.equal(stanza.node, 'princely_musings');
+      },
+      'should set itemID property' : function(err, stanza) {
+        assert.equal(stanza.itemID, 'bnd81g37d61f49fgn581');
       },
     },
     
@@ -66,14 +70,95 @@ vows.describe('retractParser').addBatch({
       'should not set node property' : function(err, stanza) {
         assert.isUndefined(stanza.node);
       },
+      'should not set itemID property' : function(err, stanza) {
+        assert.isUndefined(stanza.itemID);
+      },
     },
     
-    'when handling a non-IQ-set retract request': {
+    'when handling an IQ-get retract request': {
       topic: function(retractParser) {
         var self = this;
         var iq = new IQ('pubsub.shakespeare.lit', 'hamlet@denmark.lit/elsinore', 'get');
         var pubsubEl = new PubSub();
         var retractEl = new Retract('princely_musings');
+        retractEl.c('item', { id: 'bnd81g37d61f49fgn581' });
+        iq.c(pubsubEl).c(retractEl);
+        iq = iq.toXML();
+        iq.type = iq.attrs.type;
+        
+        function next(err) {
+          self.callback(err, iq);
+        }
+        process.nextTick(function () {
+          retractParser(iq, next)
+        });
+      },
+      
+      'should indicate an error' : function(err, stanza) {
+        assert.instanceOf(err, StanzaError);
+        assert.equal(err.type, 'modify');
+        assert.equal(err.condition, 'bad-request');
+      },
+    },
+    
+    'when handling a retract request without an item element': {
+      topic: function(retractParser) {
+        var self = this;
+        var iq = new IQ('pubsub.shakespeare.lit', 'hamlet@denmark.lit/elsinore', 'set');
+        var pubsubEl = new PubSub();
+        var retractEl = new Retract('princely_musings');
+        iq.c(pubsubEl).c(retractEl);
+        iq = iq.toXML();
+        iq.type = iq.attrs.type;
+        
+        function next(err) {
+          self.callback(err, iq);
+        }
+        process.nextTick(function () {
+          retractParser(iq, next)
+        });
+      },
+      
+      'should indicate an error' : function(err, stanza) {
+        assert.instanceOf(err, StanzaError);
+        assert.equal(err.type, 'modify');
+        assert.equal(err.condition, 'bad-request');
+      },
+    },
+    
+    'when handling a retract request with an item element lacking an id attribute': {
+      topic: function(retractParser) {
+        var self = this;
+        var iq = new IQ('pubsub.shakespeare.lit', 'hamlet@denmark.lit/elsinore', 'set');
+        var pubsubEl = new PubSub();
+        var retractEl = new Retract('princely_musings');
+        retractEl.c('item');
+        iq.c(pubsubEl).c(retractEl);
+        iq = iq.toXML();
+        iq.type = iq.attrs.type;
+        
+        function next(err) {
+          self.callback(err, iq);
+        }
+        process.nextTick(function () {
+          retractParser(iq, next)
+        });
+      },
+      
+      'should indicate an error' : function(err, stanza) {
+        assert.instanceOf(err, StanzaError);
+        assert.equal(err.type, 'modify');
+        assert.equal(err.condition, 'bad-request');
+      },
+    },
+    
+    'when handling a retract request without a node attribute': {
+      topic: function(retractParser) {
+        var self = this;
+        var iq = new IQ('pubsub.shakespeare.lit', 'hamlet@denmark.lit/elsinore', 'set');
+        var pubsubEl = new PubSub();
+        var retractEl = new Retract();
+        retractEl.c('item', { id: 'bnd81g37d61f49fgn581' });
         iq.c(pubsubEl).c(retractEl);
         iq = iq.toXML();
         iq.type = iq.attrs.type;
