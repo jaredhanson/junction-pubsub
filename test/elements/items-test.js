b0ce1a8b23de2c73baaeb7397c0957e7c71805e6
Jared Hanson <jaredhanson@gmail.com>
2011-09-29 21:41:47 -0700

    Add support for max_items attribute to Items element.

--- test/elements/items-test.js
+++ test/elements/items-test.js
@@ -17,5 +17,17 @@ vows.describe('Items').addBatch({
       assert.equal(items.toXML().toString(), '<items node="princely_musings" xmlns="http://jabber.org/protocol/pubsub"/>');
     },
   },
+  
+  'when a maxItems property is set': {
+    topic: function() {
+      var items = new Items('princely_musings');
+      items.maxItems = 2;
+      return items;
+    },
+    
+    'should build correct XML string': function(items) {
+      assert.equal(items.toXML().toString(), '<items node="princely_musings" max_items="2" xmlns="http://jabber.org/protocol/pubsub"/>');
+    },
+  },
 
 }).export(module);
