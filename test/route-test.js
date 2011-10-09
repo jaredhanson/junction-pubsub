2b51fcf3c3de60f46a1735723e8ed3fd88e34514
Jared Hanson <jaredhanson@gmail.com>
2011-10-09 13:47:53 -0700

    Implement route matching for null nodes.

--- test/route-test.js
+++ test/route-test.js
@@ -25,4 +25,30 @@ vows.describe('Route').addBatch({
     },
   },
   
+  'initialize with action, null node, and callbacks': {
+    topic: function() {
+      return new Route('publish', null, [ function(){} ]);
+    },
+    
+    'should have a query property': function (route) {
+      assert.equal(route.action, 'publish');
+    },
+    'should have a node property set to null': function (route) {
+      assert.isNull(route.node);
+    },
+    'should have a callbacks property': function (route) {
+      assert.isArray(route.callbacks);
+    },
+    'should have a regexp property set to null': function (route) {
+      assert.isNull(route.regexp);
+    },
+    'should match a null node': function (route) {
+      assert.isArray(route.match(null));
+      assert.length(route.match(null), 0);
+    },
+    'should not match a node': function (route) {
+      assert.isNull(route.match('node'));
+    },
+  },
+  
 }).export(module);
